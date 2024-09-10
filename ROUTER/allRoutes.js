4dbd8ee947f8c637656c0c26808d0ca985390159
Cacahuetedu62 <rogez.aurore01@gmail.com>
2024-09-10 14:48:35 +0200

    Suite live Thomas BUREAU DU COLOMBIER

--- ROUTER/allRoutes.js
+++ ROUTER/allRoutes.js
@@ -1,16 +1,25 @@
 import Route from "./Route.js";
 
-//Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "La galerie", "/pages/galerie.html"),
-    new Route("/signin", "connexion", "/pages/auth/signin.html"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html","/js/auth/signup.js"),
-    new Route("/allResa","Vos réservations", "/pages/reservation/AllResa.html"),
-    new Route("/reserver", "Réserver","/pages/reservations/reserver.html"),
-    new Route("/AnnulationReservation", "Annulation de la réservation", "/pages/reservation/AnnulationReservation.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html"), 
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
-    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
+ 
+//route au dessus ok
+
+    new Route("/signin", "connexion", "/Pages/auth/signin.html"),
+   
+    new Route("/signup", "Inscription", "/pages/auth/signup.html","/js/auth/signup.js"),
+    new Route("/editPassword", "Changement de mot de passe", "/Pages/auth/editPassword.html"),
+//Arrive bien à la page connexion mais pas de mise en forme, navbar, photos etc...
+ 
+
+   new Route("/reserver", "Réserver","/pages/reservations/reserver.html"),
+   //Mise en page ok, bouton annuler pas ok, il ne redirige pas vers "annulion de la reservation" AnnulationReservation.html
+    new Route("/AnnulationReservation", "Annulation de la réservation", "/Pages/reservations/AnnulationReservation.html"),
+    
+    new Route("/allResa","Vos réservations", "/Pages/reservations/AllResa.html"),
+    //Ne trouve pas la route
 ]
 
 
